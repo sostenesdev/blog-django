79c740387ce76fa0daa4f5fa9b5fc1987ab405f1
Sostenes Dias De Oliveira Junior <sostenes.junior@serra.local>
2020-08-28 09:36:22 -0300

    correção do do model que não estava rodando corretamente

--- website/models.py
+++ website/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django_soft_deletion.models import SoftDeletionModel
+# from django_soft_deletion.models import SoftDeletionModel
 
 # Create your models here.
 
 # class Post(models.Model):
-class Post(SoftDeletionModel):
+class Post(models.Model):
     title = models.CharField(max_length=200)
     sub_title = models.CharField(max_length=200)
     content = models.TextField()
